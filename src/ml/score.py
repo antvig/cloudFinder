f4570d76fe6cb10a178520b302af6dae4ae511d6
Antoine Vignon <antoine.vignon@se.com>
2021-04-06 14:25:34 +0200

    update score for DL

--- src/ml/score.py
+++ src/ml/score.py
@@ -1,43 +1,50 @@
-from sklearn.metrics import (
-    confusion_matrix,
-    accuracy_score,
-    recall_score,
-    precision_score,
-)
-
+from sklearn.metrics import jaccard_score, accuracy_score, recall_score, precision_score
+from tqdm import tqdm
 import pandas as pd
-
 
 score_fct = {
     "accuracy": accuracy_score,
     "recall": recall_score,
     "precision": precision_score,
+    "iou": jaccard_score,
 }
 
 
-def compute_classification_score(
-    df, target, pred_label, score=["accuracy", "recall", "precision"], gp_by=None
+def compute_segmentation_score(
+    y, y_pred, metrics=["accuracy", "recall", "precision", "iou"], group=False
 ):
+    """
+    Compute metrics for segmentation tasks.
+    y has shape (nbr_img, size_height, size_width)
 
-    if type(pred_label) == str:
-        pred_label = [pred_label]
+    Parameters
+    ----------
+    y
+    y_pred
+    metrics
+    group
 
-    if type(score) == str:
-        score = [score]
+    Returns
+    -------
 
-    if gp_by is None:
-        df['gp'] = 0
-        _tmp = df.groupby('gp')
+    """
+
+    out = []
+    if group is False:
+        for idx in tqdm(range(len(y))):
+            tmp_y = y[idx]
+            tmp_y_pred = y_pred[idx]
+            score = [idx]
+            for metric in metrics:
+                score.append(score_fct[metric](tmp_y.ravel(), tmp_y_pred.ravel()))
+            out.append(score)
+
+        return pd.DataFrame(out, columns=["idx"] + metrics)
     else:
-        _tmp = df.groupby(gp_by)
-    score = _tmp.apply(
-        lambda x: pd.Series(
-            {(v, s): score_fct[s](x[target], x[v]) for v in pred_label for s in score}
-        )
-    )
-    
-    if gp_by is None:
-        del df['gp']
-        score.reset_index(drop=True, inplace=True)
-    
-    return score
+        score = []
+        for metric in metrics:
+            score.append(score_fct[metric](y.ravel(), y_pred.ravel()))
+
+        out.append(score)
+
+        return pd.DataFrame(out, columns=metrics)
